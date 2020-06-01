754bea7c11ed1b0f362faa2c6aadaa8bb65b6784
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-06-01 17:16:36 +1000

    Setup benchmarks TKF style

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,31 +1,9 @@
-using BenchmarkTools, NTupleKmers, BioSequences
+using BenchmarkTools
 
 SUITE = BenchmarkGroup()
 
-SUITE["creation"] = BenchmarkGroup(["constructors"])
-
-# Add some benchmarks to the "creation" group
-dnaseq = randdnaseq(63)
-SUITE["creation"]["63mer"] = @benchmarkable DNAKmer{63,2}($dnaseq)
-
-
-
-
-
-
-
-
-
-
-
-onem   = NTupleKmers.DNAKmer{31, 1}(LongSequence{DNAAlphabet{2}}(randdnaseq(31)))
-twom   = NTupleKmers.DNAKmer{63, 2}(LongSequence{DNAAlphabet{2}}(randdnaseq(63)))
-threem = NTupleKmers.DNAKmer{95, 3}(LongSequence{DNAAlphabet{2}}(randdnaseq(95)))
-fourm  = NTupleKmers.DNAKmer{127, 4}(LongSequence{DNAAlphabet{2}}(randdnaseq(127)))
-
-
-m = NTupleKmers.DNAKmer{63, 2}(dnaseq)
-oldm = BigDNAMer{63}(dnaseq)
-
-@benchmark shiftright($m)
-@benchmark shiftright($oldm)
+for file in readdir(@__DIR__)
+    if startswith(file, "bench_") && endswith(file, ".jl")
+        SUITE[file[length("bench_") + 1:end - length(".jl")]] = include(file)
+    end
+end
